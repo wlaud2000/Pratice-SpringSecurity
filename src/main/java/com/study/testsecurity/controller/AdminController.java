75fd0fb73cbbc7a0499adeb8939823b7418ae0c8
Jimyeong Kim <kjm021221@icloud.com>
2024-04-06 23:45:16 +0900

    feat: 특정 경로에 대한 인가작업 진행
    +config.SecurityConfig 클래스 생성 후 특정 경로에 대한 인가작업 진행
    +admin.mustache 생성 후 AdminController에 URL mapping

--- src/main/java/com/study/testsecurity/controller/AdminController.java
+++ src/main/java/com/study/testsecurity/controller/AdminController.java
@@ -1,14 +1,2 @@
-package com.study.testsecurity.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class AdminController {
-
-    @GetMapping(value = "/admin")
-    public String adminP(){
-
-        return "admin";
-    }
+package com.study.testsecurity.controller;public class AdminController {
 }

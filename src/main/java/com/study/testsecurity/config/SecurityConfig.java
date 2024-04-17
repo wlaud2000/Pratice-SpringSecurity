7e8cf79e12692d2fc2fb735019be6e93c1cd50d3
Jimyeong Kim <kjm021221@icloud.com>
2024-04-17 21:09:47 +0900

    feat: csrf enable 설정
    + csrf 설정을 disable에서 enable로 변경
    + csrf 란 요청을 위조하여 사용자가 원하지 않아도 서버측으로 특정요청을 강제로 보내는 방식
    + API 서버의 경우 csrf.disable로 설정해도 무관(세션을 STATELESS로 관리하기 때문에)

--- src/main/java/com/study/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/study/testsecurity/config/SecurityConfig.java
@@ -32,8 +32,8 @@ public class SecurityConfig {
                         .loginProcessingUrl("/loginProc") //프론트단에서 loginPrecessingUrl에 설정해준 /loginProc로 login데이터를 넘기면 /loginProc로 특정한 Security가 받아서 로그인처리를 진행한다.
                         .permitAll() //이 경로는 아무나 들어올 수 있다.
                 );
-        http
-                .csrf((auth) -> auth.disable()); //csrf : 사이트 간 위조 요청 -> Spring Security는 기본 설정 되어있음
+//        http
+//                .csrf((auth) -> auth.disable()); //csrf : 사이트 간 위조 요청 -> Spring Security는 기본 설정 되어있음
                                                 //csrf설정이 동작되면 post요청을 보낼 때 csrf 토큰도 보내줘야 login이 진행된다.
                                                 //근데 csrf를 켜놓으면 개발 환경에서 토큰을 보내지 않으면 login이 진행되지 않기 때문에 잠시 disable시켜줌.
 
